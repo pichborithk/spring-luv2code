984276d6864103ac923f93b97cc049c735fa3099
Pichborith Kong <pichborith@gmail.com>
2023-12-07 23:43:40 -0800

    add section 9 - advanced jpa

---
+++ 09-advanced-hibernate-jpa/01-one-to-one-uni/src/main/java/dev/pichborith/cruddemo/dao/AppDAO.java
@@ -0,0 +1,12 @@
+package dev.pichborith.cruddemo.dao;
+
+import dev.pichborith.cruddemo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+}
---
+++ 09-advanced-hibernate-jpa/01-one-to-one-uni/src/test/java/dev/pichborith/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pichborith.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
