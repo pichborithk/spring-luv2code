97e26cd0efa5ce8dcbc501c4453adf0e02f91448
Pichborith Kong <pichborith@gmail.com>
2024-07-31 22:31:23 -0700

    add many to many

---
+++ 09-advanced-hibernate-jpa/05-many-to-many/src/main/java/dev/pichborith/cruddemo/dao/AppDAO.java
@@ -0,0 +1,36 @@
+package dev.pichborith.cruddemo.dao;
+
+import dev.pichborith.cruddemo.entity.Course;
+import dev.pichborith.cruddemo.entity.Instructor;
+import dev.pichborith.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstactorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+
+    void update(Instructor tempInstructor);
+
+    Course findCourseById(int theId);
+
+    void update(Course tempCourse);
+
+    void deleteCourseById(int theId);
+
+    void save(Course theCourse);
+
+    Course findCourseAndReviewsByCourseId(int theId);
+}
