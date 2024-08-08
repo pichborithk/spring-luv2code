94a1b728fa76d8db63a1dafbeb3b3c948d394a39
Pichborith Kong <pichborith@gmail.com>
2024-08-07 20:07:05 -0700

    update folder name

---
+++ 5-spring-boot-rest-api-security/01-spring-boot-rest-security-jdbc-authentication-plain-text/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,60 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails john = User.builder().username("john").password("{noop}123")
+                               .roles("EMPLOYEE").build();
+
+        UserDetails mary = User.builder().username("mary").password("{noop}123")
+                               .roles("EMPLOYEE", "MANAGER").build();
+
+        UserDetails susan = User.builder().username("susan")
+                                .password("{noop}123")
+                                .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+            configurer ->
+                configurer
+                    .requestMatchers(HttpMethod.GET, "/api/employees")
+                    .hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.GET, "/api/employees/**")
+                    .hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.POST, "/api/employees")
+                    .hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.PUT, "/api/employees")
+                    .hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.DELETE, "/api/employees/**")
+                    .hasRole("ADMIN")
+        );
+
+        // use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery(CSRF)
+        // in general, not required for stateless REST APIs that use POST, PUT/PATCH and DELETE
+//        http.csrf(csrf -> csrf.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
